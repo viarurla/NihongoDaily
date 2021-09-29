3a7a9652365ce71d17cb18f28868043ce4290341
Oli Jacobs <oliverjacobs@protonmail.com>
2021-09-29 18:02:07 +0100

    Initial Commit

---
+++ NihongoDaily/NihongoDailyApp.swift
@@ -0,0 +1,20 @@
+//
+//  NihongoDailyApp.swift
+//  NihongoDaily
+//
+//  Created by Oli Jacobs on 29/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct NihongoDailyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
